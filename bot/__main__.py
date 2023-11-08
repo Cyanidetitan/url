d5f63e459076f36c476e43f53fb77818e2a46641
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-11-08 14:52:31 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -95,7 +95,7 @@ async def _(e):
 async def _(e):
     await encod(e)
 
-encode_id = -1001533463219
+encode_id = -1002100546607
 async def something():
     for i in itertools.count():
         try:
@@ -135,7 +135,7 @@ async def something():
                 kk = dl.split("/")[-1]
                 aa = kk.split(".")[-1]
                 rr = "encode"
-                bb = kk.replace("@Anime_Gallery", "@Animejinx")
+                bb = kk.replace("@AnimeFiles", "@NotAnimejinx")
                 out = f"{rr}/{bb}"
                 thum = "thumb.jpg"
                 dtime = ts(int((es - s).seconds) * 1000)
